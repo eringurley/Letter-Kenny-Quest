191af0734d6ffb7bab94f75f9c3e1f4dd08f4f56
Erin Gurley <erinmgurley@gmail.com>
2019-05-09 08:59:20 -0700

    created new js and test pages, created the create-quest-link test page

---
+++ tests/create-quest-link.test.js
@@ -0,0 +1,20 @@
+// import createQuestLink from '../src/'map/create-quest-createQuestLink.js'
+const test = QUnit.test;
+
+QUnit.module('create quest link');
+
+test('create correct html', (assert)  => {
+    //arrange
+    const quest {
+        id: 'barn',
+        title: 'Barn'
+    };
+
+    const expected = '<a class="barn" href="quest.html?id=barn">></a>';
+
+    //act
+    const dom = createQuestLink(quest);
+
+    //assert
+    assert.eqaul(dom.outerHTML, expected);
+});
